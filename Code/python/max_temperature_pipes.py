25c07e4bd3fcce1afeee06c5a07412ca8a24c2db
Praveen Sripati <praveensripati@gmail.com>
2014-11-14 15:13:35 +0530

    Creates different folders for different languages

--- Code/python/max_temperature_pipes.py
+++ Code/python/max_temperature_pipes.py
@@ -13,7 +13,7 @@ sc.addFile("/home/bigdatavm/Code/Spark/filter_weather_records.rb")
 weatherData = sc.textFile("hdfs://localhost:9000/user/bigdatavm/input")
 
 #Transform the data to extract/filter and then find the max temperature
-max_temperature_per_year = weatherData.pipe("filter_weather_records.rb").map(lambda x: (x.split("\t")[0], x.split("\t")[1])).reduceByKey(lambda a,b : a if int(a) > int(b) else b).coalesce(1)
+max_temperature_per_year = weatherData.pipe("../ruby/filter_weather_records.rb").map(lambda x: (x.split("\t")[0], x.split("\t")[1])).reduceByKey(lambda a,b : a if int(a) > int(b) else b).coalesce(1)
 
 #Save the RDD back into HDFS
 max_temperature_per_year.saveAsTextFile("hdfs://localhost:9000/user/bigdatavm/output")
